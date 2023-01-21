5efacde692cafa0374ec4de1b2b53f81b0ab3065
suna-kawa <sunagawa69852@gmail.com>
2023-01-21 10:39:25 +0900

    validationの実装

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,12 @@
 class Plan < ApplicationRecord
+  validates :title, :start_date, :end_date, presence: true
+  validates :title, length: { maximum: 20 }
+  validates :memo,  length: { maximum: 500 }
+  validate :start_end_check
+
+  def start_end_check
+    if self.end_date < self.start_date
+      errors.add(:end_date, "開始日より前の日付を指定する事は出来ません")
+    end
+  end
 end
